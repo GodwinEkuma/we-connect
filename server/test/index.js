a68028880d259c64807105c801c7713db7cc8c1b
Godwin Ekuma <godwinekuma@gmail.com>
2018-03-10 17:04:08 +0100

    added test for GET business/id

--- server/test/index.js
+++ server/test/index.js
@@ -18,3 +18,26 @@ describe('GET business', () => {
       });
   });
 });
+
+// Test for getting a particular business
+describe('GET business/id', () => {
+  it('it should return business by id', (done) => {
+    chai.request(app)
+      .get('/v1/businesses/4')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('businessName');
+        expect(res.body).to.have.property('businessCategory');
+        expect(res.body).to.have.property('businessAddress');
+        done();
+      });
+  });
+  it('it should return 404 if business is not found', (done) => {
+    chai.request(app)
+      .get('/v1/businesses/8')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+});
