2dfbf2eb007404f97f50627910e82799dcd06b20
Godwin Ekuma <godwinekuma@gmail.com>
2018-03-12 14:16:36 +0100

    fixes issues from code climate

--- server/data/business.js
+++ server/data/business.js
@@ -18,17 +18,17 @@ const BusinessData = [
       },
       {
         id: 2,
-        product: 'UI/UX',
+        product: 'Web Design',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       },
       {
         id: 3,
-        product: 'UI/UX',
+        product: 'Web Development',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       },
       {
         id: 4,
-        product: 'UI/UX',
+        product: 'Graphics Design',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       }
 
@@ -36,9 +36,9 @@ const BusinessData = [
   },
   {
     id: 2,
-    businessName: 'Instrap Solutions Limited',
-    businessPhone: '0133512053',
-    businessEmail: 'info@instrap.com',
+    businessName: 'Food Trolley Limited',
+    businessPhone: '08134727635',
+    businessEmail: 'info@food.com',
     businessCategory: 'Food Services',
     businessWebsite: 'https//:instrap.com',
     businessDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
@@ -48,22 +48,22 @@ const BusinessData = [
     products: [
       {
         id: 1,
-        product: 'UI/UX',
+        product: 'Grecory Delivery',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
       },
       {
         id: 2,
-        product: 'UI/UX',
+        product: 'Fresh Food Delivery',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       },
       {
         id: 3,
-        product: 'UI/UX',
+        product: 'Pepper Delivery',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       },
       {
         id: 4,
-        product: 'UI/UX',
+        product: 'Noodles and Pasta Delivery',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       }
 
@@ -71,9 +71,9 @@ const BusinessData = [
   },
   {
     id: 3,
-    businessName: 'Instrap Solutions Limited',
+    businessName: 'InfoMall Limited',
     businessPhone: '0133512053',
-    businessEmail: 'info@instrap.com',
+    businessEmail: 'info@infomall.com',
     businessCategory: 'Web Development and Graphics',
     businessWebsite: 'https//:instrap.com',
     businessDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
@@ -83,22 +83,22 @@ const BusinessData = [
     products: [
       {
         id: 1,
-        product: 'UI/UX',
+        product: 'Project Management',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
       },
       {
         id: 2,
-        product: 'UI/UX',
+        product: 'Business Consultancy',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       },
       {
         id: 3,
-        product: 'UI/UX',
+        product: 'CCTV Installation',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       },
       {
         id: 4,
-        product: 'UI/UX',
+        product: 'Technology',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       }
 
@@ -106,10 +106,10 @@ const BusinessData = [
   },
   {
     id: 4,
-    businessName: 'Instrap Solutions Limited',
+    businessName: 'Emmafids Limited',
     businessPhone: '0133512053',
-    businessEmail: 'info@instrap.com',
-    businessCategory: 'Food Services',
+    businessEmail: 'info@emmafids.com',
+    businessCategory: 'Printing Services',
     businessWebsite: 'https//:instrap.com',
     businessDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     businessAddress: '22a Prof. Kumi , Lekki, Lagos.',
@@ -118,22 +118,22 @@ const BusinessData = [
     products: [
       {
         id: 1,
-        product: 'UI/UX',
+        product: 'Packaging',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
       },
       {
         id: 2,
-        product: 'UI/UX',
+        product: 'Light Packaging',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       },
       {
         id: 3,
-        product: 'UI/UX',
+        product: 'General Printing',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       },
       {
         id: 4,
-        product: 'UI/UX',
+        product: 'Pre Press',
         Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
       }
 
