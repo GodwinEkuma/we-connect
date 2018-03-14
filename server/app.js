d8d973e0f3672ab5b541271298921b567b5096e2
Godwin Ekuma <godwinekuma@gmail.com>
2018-03-14 15:42:30 +0100

    adds api documentation

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './docs/swagger.json';
 import Business from './routes/business';
 import Users from './routes/users';
 
 const app = express();
+
+// enable cors
+app.use(cors());
 
 // Configure middlewares
 app.use(logger('dev'));
@@ -12,6 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // configure the routes
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to we-connect where businesses and humans meet' });
 });
